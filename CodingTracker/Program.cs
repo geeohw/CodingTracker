6fc92185038546b8162f0419a9be072d66c17929
geeohw <garias1214@gmail.com>
2025-10-17 10:31:33 -0400

    Console.WriteLine modified

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, Geo !!");
+Console.WriteLine("This is just for practice!!");
